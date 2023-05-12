596146e493cd05060581ac9ac43ad7403b4fa99c
Thomas Fossati <thomas.fossati@arm.com>
2023-05-12 11:31:43 +0100

    initial sketch of the demo orchestrator
    Signed-off-by: Thomas Fossati <thomas.fossati@arm.com>

---
+++ demo/build/veraison.mk
@@ -0,0 +1,10 @@
+
+VERAISON_ROOT := .veraison-services
+
+$(VERAISON_ROOT):
+	git clone https://github.com/veraison/services $@
+
+.PHONY: veraison-build-containers
+veraison-build-containers: $(VERAISON_ROOT)
+	@docker network rm veraison-net || true
+	$(MAKE) -C "$</deployments/docker" deploy services
