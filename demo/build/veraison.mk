9c8a5335c2de4371aba1738d9aaedc3e948a6932
Thomas Fossati <tho.ietf@gmail.com>
2023-05-18 16:55:04 +0100

    Revert "add veraison containers build"
    This reverts commit 2259699103a596b1b28fd3c3cb59a7a2173a9323.

--- demo/build/veraison.mk
+++ demo/build/veraison.mk
@@ -1,11 +1,1 @@
 # TODO(tho) clone and build
-
-VERAISON_ROOT := .veraison-services
-
-$(VERAISON_ROOT):
-	git clone https://github.com/veraison/services $@
-
-.PHONY: veraison-build-containers
-veraison-build-containers: $(VERAISON_ROOT)
-	@docker network rm veraison-net || true
-	$(MAKE) -C "$</deployments/docker" deploy services
