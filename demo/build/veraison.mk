57898ecd5393d6c8cc97b0bfeae840c132a466e2
Thomas Fossati <thomas.fossati@arm.com>
2023-05-23 11:32:48 +0100

    fix docker-compose startup ordering
    Signed-off-by: Thomas Fossati <thomas.fossati@arm.com>

--- demo/build/veraison.mk
+++ demo/build/veraison.mk
@@ -1,10 +1,17 @@
-
 VERAISON_ROOT := .veraison-services
 
+# The command line can override VERAISON_BRANCH to point to a branch other than
+# main
+VERAISON_BRANCH ?= main
+
 $(VERAISON_ROOT):
-	git clone https://github.com/veraison/services $@
+	git clone --branch $(VERAISON_BRANCH) https://github.com/veraison/services $@
 
 .PHONY: veraison-build-containers
 veraison-build-containers: $(VERAISON_ROOT)
 	@docker network rm veraison-net || true
 	$(MAKE) -C "$</deployments/docker" deploy services
+
+.PHONY: veraison-destroy-containers
+veraison-destroy-containers:
+	$(MAKE) -C "$(VERAISON_ROOT)/deployments/docker" really-clean
