52a5234cf3e185dd9c1f3638e0bfdd55113fe68a
Yogesh Deshpande <yogesh.deshpande@arm.com>
2023-10-24 06:25:11 -0400

    Demo specific Veraison Deployment
    Signed-off-by: Yogesh Deshpande <yogesh.deshpande@arm.com>

--- demo/build/veraison.mk
+++ demo/build/veraison.mk
@@ -1,4 +1,5 @@
 VERAISON_ROOT := .veraison-services
+THIS_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 # The command line can override VERAISON_BRANCH to point to a branch other than
 # main
@@ -10,6 +11,7 @@ $(VERAISON_ROOT):
 .PHONY: veraison-build-containers
 veraison-build-containers: $(VERAISON_ROOT)
 	@docker network rm veraison-net || true
+	cp $(THIS_DIR)/veraison_deployment.cfg.yml $(VERAISON_ROOT)/deployments/docker/src/config.yaml.template
 	$(MAKE) -C "$</deployments/docker" deploy services
 
 .PHONY: veraison-destroy-containers
